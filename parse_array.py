401d168eb52ebaef48c3c92a30de144c1c15280f
John Bailey <john.e.bailey@gmail.com>
2021-03-02 20:26:42 +0000

    debugged parse_array script from checkio. also made my own solution that is cleaner.

--- parse_array.py
+++ parse_array.py
@@ -8,7 +8,7 @@ Elements of the array are separated by commas
 # my solution
 # works!
 from ast import literal_eval
-def parse_array(input: str) -> list:
+def my_parse_array(input: str) -> list:
     try:
         data = literal_eval(input.strip())
         sehr_gut = True
@@ -18,18 +18,18 @@ def parse_array(input: str) -> list:
         raise ValueError
     return data
 
-assert parse_array("[1, 2, 3]") == [1, 2, 3]
-assert parse_array("[[1], 2, 3]") == [[1], 2, 3]
-assert parse_array("[-3, [-2, 0], 10]") == [-3, [-2, 0], 10]
+assert my_parse_array("[1, 2, 3]") == [1, 2, 3]
+assert my_parse_array("[[1], 2, 3]") == [[1], 2, 3]
+assert my_parse_array("[-3, [-2, 0], 10]") == [-3, [-2, 0], 10]
 
 try:
-    parse_array("[asd]")
+    my_parse_array("[asd]")
     assert False, "Only integers"
 except ValueError:
     pass
 
 try:
-    parse_array("[2, 3]]")
+    my_parse_array("[2, 3]]")
     assert False, "Excess bracket"
 except ValueError:
     pass
@@ -53,9 +53,9 @@ def parse_array(s, _w=WHITESPACE_STR, _sep=","):
             continue
         if ch == "[":
             if started_flag and not stack:
-                raise ValueError("Wrong string.")
+                raise ValueError(f'Wrong string: {ch}, not started_flag and not stack')
             if closed_flag or accumulator:
-                raise ValueError
+                raise ValueError(f'closed_flag or accumulator')
             in_array = []
             if stack:
                 stack[-1](in_array)
@@ -64,15 +64,16 @@ def parse_array(s, _w=WHITESPACE_STR, _sep=","):
                 started_flag = True
             stack.append(in_array.append)
         elif not started_flag:
-            raise ValueError("Wrong string.")
+            raise ValueError(f'Wrong string: {ch}, not started_flag')
         elif ch == "]":
-
             if not stack:
-                raise ValueError("Wrong string.")
+                raise ValueError(f'Wrong string: {ch}, not stack')
             if accumulator:
                 stack[-1](int(accumulator))
                 accumulator = ""
             stack.pop()
+            if not stack:
+                started_flag = False
             closed_flag = True
             sep_flag = False
             whitespace_flag = False
@@ -83,19 +84,44 @@ def parse_array(s, _w=WHITESPACE_STR, _sep=","):
             elif closed_flag:
                 pass
             else:
-                raise ValueError("Wrong string.")
+                raise ValueError(f'Wrong string: {ch}, _sep but not accumulator and not closed_flag')
             sep_flag = True
             closed_flag = False
             whitespace_flag = False
         else:
             if whitespace_flag and accumulator or closed_flag:
-                raise ValueError
+                raise ValueError(f'whitespace_flag: {whitespace_flag} and accumulator {accumulator} or closed_flag {closed_flag}')
             accumulator += ch
         whitespace_flag = False
+    if started_flag:
+        raise ValueError(f'extra brackets')
     if not array is None:
         return array
     else:
-        raise ValueError("Wrong string")
+        raise ValueError(f'array is None')
+
+
+# fails
+# returning "[3]" instead of ValueError
+try:
+    parse_array("[3],")
+    assert False, "Excess separator"
+except ValueError:
+    pass
+
+# fails
+# returning "[[2]]" instead of ValueError
+try:
+    parse_array("[[2]")
+    assert False, "Excess opened bracket"
+except ValueError:
+    pass
+
+# testing
+parse_array("[1, 2, 3],") == [1, 2, 3]
+
+parse_array("[[1], 2, 3]") == [[1], 2, 3]
+
 
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
@@ -118,5 +144,73 @@ except ValueError:
 try:
     parse_array("[2, 3]]")
     assert False, "Excess bracket"
+except ValueError:
+    pass
+
+try:
+    parse_array("[++2, 1]")
+    assert False, "Two plus"
+except ValueError:
+    pass
+
+try:
+    parse_array("[10, 11, , 12]")
+    assert False, "Two separators"
+except ValueError:
+    pass
+
+try:
+    parse_array(" 13 ")
+    assert False, "Where is a list?"
+except ValueError:
+    pass
+
+# fails
+try:
+    parse_array("[[2]")
+    assert False, "Excess opened bracket"
+except ValueError:
+    pass
+
+try:
+    parse_array("[3 4]")
+    assert False, "Check for spurious spaces within a number"
+except ValueError:
+    pass
+
+try:
+    parse_array("[10, 11,, 12]")
+    assert False, "Check for double separators without a space in between"
+except ValueError:
+    pass
+
+try:
+    parse_array("[[]3]")
+    assert False, "Check for missing separators after []"
+except ValueError:
+    pass
+
+try:
+    parse_array("[2[]]")
+    assert False, " Check for missing separators before []"
+except ValueError:
+    pass
+
+# fails
+try:
+    parse_array("[3],")
+    assert False, "Excess separator"
+except ValueError:
+    pass
+
+try:
+    parse_array("[1,2]3")
+    assert False, "Excess number"
+except ValueError:
+    pass
+
+try:
+    parse_array("[1], [2,3]")
+    assert False, "Here should be only one array."
 except ValueError:
     pass
