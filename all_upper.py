b32ded9442b5c49c6e7f2d33dff0b42676052fa4
John Bailey <john.bailey@omf.com>
2024-03-22 20:59:14 +0000

    testing update

--- all_upper.py
+++ all_upper.py
@@ -1,5 +1,5 @@
 '''
-Check if a given string has all symbols in upper case.
+CHECK if a given string has all symbols in upper case.
 If the string is empty or doesn't have any letter in it - function should return False.
 '''
 def is_all_upper(text: str) -> bool:
